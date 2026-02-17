c2f684cfd257556c5d5d2321efeda51960f7ccb8
carlpascual1818 <carlpascual1818@gmail.com>
2026-02-18 04:22:56 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 // Put your Supabase project settings here.
 window.APP_CONFIG = {
-  SUPABASE_URL: "https://YOURPROJECT.supabase.co",
-  SUPABASE_ANON_KEY: "YOUR_ANON_KEY"
+  SUPABASE_URL: "https://wkwgyoowhfhkyqwlufup.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indrd2d5b293aGZoa3lxd2x1ZnVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEzNDQwMTIsImV4cCI6MjA4NjkyMDAxMn0.ArSqtBC9K_268UnAfwTiqgYoJXGoXDmp1n9J2ISzgLE"
 };
